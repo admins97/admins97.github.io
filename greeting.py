eb0b7784657ea214b012d488fdc4bfc4d35fa2d1
admins97 <alstjr88@g.skku.edu>
2023-05-28 06:11:38 +0900

    error fix

--- greeting.py
+++ greeting.py
@@ -1,3 +1,11 @@
 import os
 
-print("Hi {}th visitor! Welcome to admins97's Homepage!".format(int(os.environ["VISIT_NUM"])))
+def check(n: str):
+    if n == 'Minsu':
+        print("Hi {}! Welcome to admins97's Homepage!".format(n))
+        return True
+    else:
+        return False
+    
+def test_check():
+    assert check(str(os.environ["VISIT_NUM"])) == True
